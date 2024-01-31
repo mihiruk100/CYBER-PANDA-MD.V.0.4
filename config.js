2f94de578ddd0859c932aeb9bdcf127f2464bc9a
mihiruk100 <155881114+mihiruk100@users.noreply.github.com>
2024-01-31 15:11:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,12 +21,12 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
+global.Owner = ["+94701415046", "+94778764083"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+94701415046", "+94778764083"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94778764083"];
+global.OwnerName = "MIHIRUK";
+global.BotName = "CYBER PANDA";
+global.packname = "🦋 CYBER PANDA DEPLOYE BY MIHIRUK 🦋";                             //Do not change.
 global.author = "KUSHAN";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
